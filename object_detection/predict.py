ecbad4a1fd31cf0dd4ee49780ee571f8983020da
Hisham Abdel Qader <hisham_abdelqader@yahoo.com>
2018-11-20 11:53:05 +1300

    Added:     - Continuous Mode (flag)     - Compatibility with png (in addition to jpg)     - Score threshold Moved prediction code into methods

--- object_detection/predict.py
+++ object_detection/predict.py
@@ -2,28 +2,34 @@
 # https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
 # Licensed under Apache 2.0 (Most likely, since tensorflow is Apache 2.0)
 # Modifications Copyright (C) 2018 University of Waikato, Hamilton, NZ
+#
+# Performs predictions on all images present in the folder passed as "prediction_in", then outputs the results as
+# csv files in the folder passed as "prediction_out"
+#
+# Score threshold can be specified (passed as a parameter) to ignore all rois with low score
+# Can run in a continuous mode, where it will run indefinitely
 
 import numpy as np
 import os
 import sys
 import tensorflow as tf
 import argparse
-from glob import glob
-
 from PIL import Image
+from tensorflow import Graph
 
-# This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
 from object_detection.utils import ops as utils_ops
-
 from object_detection.utils import label_map_util
 
+# Number of classes
+NUM_CLASSES = 5
 
+# Method to convert the image into a numpy array
 def load_image_into_numpy_array(image):
     (im_width, im_height) = image.size
     return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
 
-
+# This is where the actual prediction occur
 def run_inference_for_single_image(image, graph):
     with graph.as_default():
         with tf.Session() as sess:
@@ -70,78 +76,104 @@ def run_inference_for_single_image(image, graph):
                 output_dict['detection_masks'] = output_dict['detection_masks'][0]
     return output_dict
 
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--graph', help='Path to the frozen detection graph', required=True, default=None)
-    parser.add_argument('--labels', help='Path to the label map', required=True, default=None)
-    parser.add_argument('--prediction_in', help='Path to the test images', required=True, default=None)
-    parser.add_argument('--prediction_out', help='Path to the output csv files folder', required=True, default=None)
-    parser.add_argument('--score', type=float, help='Score threshold to include in csv file', required=False, default=0.0)
-    args = vars(parser.parse_args())
-
-    # Path to frozen detection graph. This is the actual model that is used for the object detection.
-    PATH_TO_FROZEN_GRAPH = args['graph']
-
-    # List of the strings that is used to add correct label for each box.
-    PATH_TO_LABELS = args['labels']
-
-    NUM_CLASSES = 1
-
-    score_threshold = args['score']
-
-    detection_graph = tf.Graph()
+# Loads the provided frozen graph into detection_graph to use with prediction
+def load_frozen_graph(frozen_graph_path):
+    detection_graph = tf.Graph()  # type: Graph
     with detection_graph.as_default():
         od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+        with tf.gfile.GFile(frozen_graph_path, 'rb') as fid:
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
+    return detection_graph
 
-    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+# Method performing predictions on all images ony by one
+def predict_on_images(test_images_directory, detection_graph, output_path, score_threshold, categories):
+    image_no = 1    # A variable used only to print out the progress
+    # Iterate through all files present in "test_images_directory"
+    for image_path in os.listdir(test_images_directory):
+        print("Processing image {}\n".format(image_no))
+        image_no += 1
+        # Load images only, currently supporting only jpg and png
+        if image_path.lower().endswith(".jpg") or image_path.lower().endswith(".png"):
+            image = Image.open(os.path.join(test_images_directory, image_path))
+            width, height = image.size
+
+            # Remove alpha channel if present
+            if image_path.lower().endswith(".png") and (image.mode is 'RGBA' or 'ARGB'):
+                image = image.convert('RGB')
+            # Convert the image into a numpy array
+            image_np = load_image_into_numpy_array(image)
+            # Actual detection
+            output_dict = run_inference_for_single_image(image_np, detection_graph)
+
+            # Loading results
+            boxes = output_dict['detection_boxes']
+            scores = output_dict['detection_scores']
+            classes = output_dict['detection_classes']
+
+            # Writing results into a csv file, storing it in "output_path" directory
+            roi_path = "{}/{}-rois.csv".format(output_path, os.path.splitext(os.path.basename(image_path))[0])
+            with open(roi_path, "w") as roi_file:
+                # File header
+                roi_file.write("file,x0,y0,x1,y1,label,label_str,score\n")
+                for index in range(output_dict['num_detections']):
+                    y0, x0, y1, x1 = boxes[index]
+                    label = classes[index]
+                    label_str = categories[label - 1]['name']
+                    score = scores[index]
+
+                    # Ignore this roi if the score is less than the provided threshold
+                    if score < score_threshold:
+                        continue
+
+                    # Translate roi coordinates into image coordinates
+                    x0 = x0 * width
+                    y0 = y0 * height
+                    x1 = x1 * width
+                    y1 = y1 * height
+
+                    roi_file.write(
+                        "{},{},{},{},{},{},{},{}\n".format(os.path.basename(image_path), x0, y0, x1, y1, label, label_str,
+                                                           score))
+
+if __name__ == '__main__':
+
+    # Arguments to be provided by the user
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--graph', help='Path to the frozen detection graph', required=True, default=None)
+    parser.add_argument('--labels', help='Path to the labels map', required=True, default=None)
+    parser.add_argument('--prediction_in', help='Path to the test images', required=True, default=None)
+    parser.add_argument('--prediction_out', help='Path to the output csv files folder', required=True, default=None)
+    parser.add_argument('--score', type=float, help='Score threshold to include in csv file', required=False, default=0.0)
+    parser.add_argument('--continuous', action='store_true', help='Whether to continuously load test images and perform prediction')
+    args = vars(parser.parse_args())
+
+    # Path to frozen detection graph. This is the actual model that is used for the object detection
+    frozen_graph_path = args['graph']
+    detection_graph = load_frozen_graph(frozen_graph_path)
+
+    # List of the strings that is used to add correct label for each box
+    labels_map_path = args['labels']
+
+    # The threshold of including the detected roi in the output csv
+    score_threshold = args['score']
+
+    # Getting classes strings from the label map
+    label_map = label_map_util.load_labelmap(labels_map_path)
     categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
                                                                 use_display_name=True)
-    category_index = label_map_util.create_category_index(categories)
 
-    # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-    PATH_TO_TEST_IMAGES_DIR = args['prediction_in']
-    TEST_IMAGE_PATHS = glob(os.path.join(PATH_TO_TEST_IMAGES_DIR, "*.jpg"))
+    while True:
+        # Loading the path to images to perform prediction on
+        test_images_directory = args['prediction_in']
 
-    OUTPUT_PATH = args['prediction_out']
+        # Output directory for the csv files
+        output_path = args['prediction_out']
 
-    for image_path in TEST_IMAGE_PATHS:
-        image = Image.open(image_path)
-        width, height = image.size
-        # the array based representation of the image will be used later in order to prepare the
-        # result image with boxes and labels on it.
-        image_np = load_image_into_numpy_array(image)
-        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-        image_np_expanded = np.expand_dims(image_np, axis=0)
-        # Actual detection.
-        output_dict = run_inference_for_single_image(image_np, detection_graph)
+        # Performing the prediction and producing the csv files
+        predict_on_images(test_images_directory, detection_graph, output_path, score_threshold, categories)
 
-        boxes = output_dict['detection_boxes']
-        scores = output_dict['detection_scores']
-        classes = output_dict['detection_classes']
-
-        roi_path = "{}/{}-rois.csv".format(OUTPUT_PATH, os.path.splitext(os.path.basename(image_path))[0])
-        with open(roi_path, "w") as roi_file:
-            roi_file.write("file,x0,y0,x1,y1,label,label_str,score\n")
-            for index in range(output_dict['num_detections']):
-                y0, x0, y1, x1 = boxes[index]
-                label = classes[index]
-                label_str = categories[label - 1]['name']
-                score = scores[index]
-
-                if score < score_threshold:
-                    continue
-
-                x0 = x0 * width
-                y0 = y0 * height
-                x1 = x1 * width
-                y1 = y1 * height
-
-                roi_file.write(
-                    "{},{},{},{},{},{},{},{}\n".format(os.path.basename(image_path), x0, y0, x1, y1, label, label_str,
-                                                       score))
+        # Exit if not continuous
+        if not args['continuous']:
+            break
