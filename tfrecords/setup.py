17e7a76b6fd33b1e8ccb262f99aacaf184c23d5d
Peter Reutemann <fracpete@gmail.com>
2019-01-08 14:49:40 +1300

    added setup.py; shifted requirements to setup.py

---
+++ tfrecords/setup.py
@@ -0,0 +1,35 @@
+# setup.py
+# Copyright (C) 2019 Fracpete (fracpete at waikato dot ac dot nz)
+
+from setuptools import setup
+
+
+setup(
+    name="tfrecords",
+    description="Converting ADAMS annotations to tfrecords.",
+    url="https://github.com/waikato-datamining/tensorflow/tfrecords",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Programming Language :: Python :: 3',
+    ],
+    license='MIT License',
+    packages=[
+        "adams",
+    ],
+    version="0.0.1",
+    author='Peter Reutemann',
+    author_email='fracpete@waikato.ac.nz',
+    install_requires=[
+        "cython",
+        "javaproperties",
+        "argparse",
+        "contextlib2",
+        "pillow",
+        "lxml",
+        "jupyter",
+        "matplotlib",
+        "numpy",
+    ],
+)
