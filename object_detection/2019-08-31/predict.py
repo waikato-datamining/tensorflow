0dac0787fa2c0ad81ea969945f9797d7e9fc71b6
hishamjuve <hisham_abdelqader@yahoo.com>
2019-09-06 15:29:48 +1200

    it now produces total I/O and inference time plus I/O and inference time per image\nadded status argument to produce a status text file with Success or error message

--- object_detection/2019-08-31/predict.py
+++ object_detection/2019-08-31/predict.py
@@ -87,123 +87,83 @@ def load_frozen_graph(frozen_graph_path):
     return detection_graph
 
 # Method performing predictions on all images ony by one or combined as specified by the int value of num_imgs
-def predict_on_images(test_images_directory, detection_graph, output_path, score_threshold, categories, file, num_imgs):
+def predict_on_images(test_images_directory, detection_graph, output_path, score_threshold, categories, num_imgs):
     image_no = 1    # A variable used only to print out the progress
-    time_delta_total = 0
     images_total = 0
     # Iterate through all files present in "test_images_directory"
-    while True:
-        im_list = []
-        # Loop to pick up images equal to num_imgs or the remaining images if less
-        for image_path in os.listdir(test_images_directory):
-            print("Processing image {}\n".format(image_no))
-            # Load images only, currently supporting only jpg and png
-            if image_path.lower().endswith(".jpg") or image_path.lower().endswith(".png"):
-                im_list.append(os.path.join(test_images_directory, image_path))
-                image_no += 1
-            if len(im_list) == num_imgs:
-                break
-        
-        if len(im_list) == 0:
-            break
-        
-        # Combining picked up images
-        i = len(im_list)
-        combined = []
-        if i > 1:
-            comb_file = os.path.join(output_path, "combined.png")
-            while i != 0:
-                if len(combined) == 0:
-                    img2 = Image.open(im_list[i-1])
-                    img1 = Image.open(im_list[i-2])
-                    i -= 1
-                    combined.append(comb_file)
-                else:
-                    img2 = Image.open(combined[0])
-                    img1 = Image.open(im_list[i-1])
-                i -= 1
-                # Remove alpha channel if present
-                if img1.mode is 'RGBA' or 'ARGB':
-                    img1 = img1.convert('RGB')
-                if img2.mode is 'RGBA' or 'ARGB':
-                    img2 = img2.convert('RGB')
-                w1, h1 = img1.size
-                w2, h2 = img2.size
-                comb_img = np.zeros((h1+h2, max(w1, w2), 3), np.uint8)
-                comb_img[:h1, :w1, :3] = img1
-                comb_img[h1:h1+h2, :w2, :3] = img2
-                comb_img = Image.fromarray(comb_img)
-                comb_img.save(comb_file)
-                
-        if len(combined) == 0:
-            im_name = im_list[0]
-        else:
-            im_name = combined[0]
+    time_file_path = os.path.join(output_path, "inference_time.csv")
+    with open(time_file_path, "w") as time_file:
+        total_time = 0
+        time_file.write("Image(s)_file_name(s),Total_time(ms),Number_of_images,Time_per_image(ms)\n")
+        while True:
+            start_time = datetime.now()
+            im_list = []
+            # Loop to pick up images equal to num_imgs or the remaining images if less
+            for image_path in os.listdir(test_images_directory):
+                # Load images only, currently supporting only jpg and png
+                if image_path.lower().endswith(".jpg") or image_path.lower().endswith(".png"):
+                    im_list.append(os.path.join(test_images_directory, image_path))
+                    print("Processing image {}\n".format(image_no))
+                    image_no += 1
+                if len(im_list) == num_imgs:
+                    break
             
-        image = Image.open(im_name)
+            if len(im_list) == 0:
+                break
+            
+            # Combining picked up images
+            i = len(im_list)
+            combined = []
+            if i > 1:
+                comb_file = os.path.join(output_path, "combined.png")
+                while i != 0:
+                    if len(combined) == 0:
+                        img2 = Image.open(im_list[i-1])
+                        img1 = Image.open(im_list[i-2])
+                        i -= 1
+                        combined.append(comb_file)
+                    else:
+                        img2 = Image.open(combined[0])
+                        img1 = Image.open(im_list[i-1])
+                    i -= 1
+                    # Remove alpha channel if present
+                    if img1.mode is 'RGBA' or 'ARGB':
+                        img1 = img1.convert('RGB')
+                    if img2.mode is 'RGBA' or 'ARGB':
+                        img2 = img2.convert('RGB')
+                    w1, h1 = img1.size
+                    w2, h2 = img2.size
+                    comb_img = np.zeros((h1+h2, max(w1, w2), 3), np.uint8)
+                    comb_img[:h1, :w1, :3] = img1
+                    comb_img[h1:h1+h2, :w2, :3] = img2
+                    comb_img = Image.fromarray(comb_img)
+                    comb_img.save(comb_file)
+                    
+            if len(combined) == 0:
+                im_name = im_list[0]
+            else:
+                im_name = combined[0]
+                
+            image = Image.open(im_name)
 
-        # Remove alpha channel if present
-        if im_name.lower().endswith(".png") and (image.mode is 'RGBA' or 'ARGB'):
-            image = image.convert('RGB')
-        # Convert the image into a numpy array
-        image_np = load_image_into_numpy_array(image)
-        # Record current time
-        time_start = datetime.now()
-        # Actual detection
-        output_dict = run_inference_for_single_image(image_np, detection_graph)
-        # Compute time needed to predict per image in ms
-        time_end = datetime.now()
-        time_delta = time_end - time_start
-        if image_no != 2:
-            time_delta_total += int(time_delta.total_seconds() * 1000)
-            images_total += 1
+            # Remove alpha channel if present
+            if im_name.lower().endswith(".png") and (image.mode is 'RGBA' or 'ARGB'):
+                image = image.convert('RGB')
+            # Convert the image into a numpy array
+            image_np = load_image_into_numpy_array(image)
+            # Actual detection
+            output_dict = run_inference_for_single_image(image_np, detection_graph)
 
-        # Loading results
-        boxes = output_dict['detection_boxes']
-        scores = output_dict['detection_scores']
-        classes = output_dict['detection_classes']
+            # Loading results
+            boxes = output_dict['detection_boxes']
+            scores = output_dict['detection_scores']
+            classes = output_dict['detection_classes']
 
-        # Original code for writing results into a csv file for the combined image, storing it in "output_path" directory (kept to verify csv's after splitting)
-        roi_path = "{}/{}-rois.csv".format(output_path, os.path.splitext(os.path.basename(im_name))[0])
-        with open(roi_path, "w") as roi_file:
-            # File header
-            roi_file.write("file,x0,y0,x1,y1,label,label_str,score\n")
-            for index in range(output_dict['num_detections']):
-                y0, x0, y1, x1 = boxes[index]
-                label = classes[index]
-                label_str = categories[label - 1]['name']
-                score = scores[index]
-
-                # Ignore this roi if the score is less than the provided threshold
-                if score < score_threshold:
-                    continue
-
-                # Translate roi coordinates into image coordinates
-                x0 = x0 * image.width
-                y0 = y0 * image.height
-                x1 = x1 * image.width
-                y1 = y1 * image.height
-
-                roi_file.write(
-                    "{},{},{},{},{},{},{},{}\n".format(os.path.basename(im_name), x0, y0, x1, y1, label, label_str,
-                                                       score))
-                                                       
-        # Code for splitting rois to multiple csv's, one csv per image before combining
-        min_height = 0
-        max_height = 0
-        prev_min = 0
-        translation = 0
-        for i in range(len(im_list)):
-            img = Image.open(im_list[i])
-            img_height = img.height
-            min_height = prev_min
-            max_height += img_height
-            prev_min = max_height
-            roi_path = "{}/{}-rois.csv".format(output_path, os.path.splitext(os.path.basename(im_list[i]))[0])
+            # Original code for writing results into a csv file for the combined image, storing it in "output_path" directory (kept to verify csv's after splitting)
+            roi_path = "{}/{}-rois.csv".format(output_path, os.path.splitext(os.path.basename(im_name))[0])
             with open(roi_path, "w") as roi_file:
                 # File header
                 roi_file.write("file,x0,y0,x1,y1,label,label_str,score\n")
-                # rois
                 for index in range(output_dict['num_detections']):
                     y0, x0, y1, x1 = boxes[index]
                     label = classes[index]
@@ -213,28 +173,75 @@ def predict_on_images(test_images_directory, detection_graph, output_path, score
                     # Ignore this roi if the score is less than the provided threshold
                     if score < score_threshold:
                         continue
-                        
-                    # Translate roi coordinates into combined image coordinates
+
+                    # Translate roi coordinates into image coordinates
                     x0 = x0 * image.width
                     y0 = y0 * image.height
                     x1 = x1 * image.width
                     y1 = y1 * image.height
-                    
-                    if y0 > max_height or y1 > max_height:
-                        continue
-                    elif y0 < min_height or y1 < min_height:
-                        continue
 
-                    # output
-                    roi_file.write("{},{},{},{},{},{},{},{}\n".format(os.path.basename(im_list[i]), x0, y0-translation, x1, y1-translation,
-                            label, label_str, score))
-            translation += img_height
-        
-        # Move finished images to output_path
-        for i in range(len(im_list)):
-            os.rename(im_list[i], os.path.join(output_path, os.path.basename(im_list[i])))
-    # Save average elapsed time to file
-    file.write(str(int(time_delta_total / images_total)) + " ms")
+                    roi_file.write(
+                        "{},{},{},{},{},{},{},{}\n".format(os.path.basename(im_name), x0, y0, x1, y1, label, label_str,
+                                                           score))
+                                                           
+            # Code for splitting rois to multiple csv's, one csv per image before combining
+            min_height = 0
+            max_height = 0
+            prev_min = 0
+            translation = 0
+            for i in range(len(im_list)):
+                img = Image.open(im_list[i])
+                img_height = img.height
+                min_height = prev_min
+                max_height += img_height
+                prev_min = max_height
+                roi_path = "{}/{}-rois.csv".format(output_path, os.path.splitext(os.path.basename(im_list[i]))[0])
+                with open(roi_path, "w") as roi_file:
+                    # File header
+                    roi_file.write("file,x0,y0,x1,y1,label,label_str,score\n")
+                    # rois
+                    for index in range(output_dict['num_detections']):
+                        y0, x0, y1, x1 = boxes[index]
+                        label = classes[index]
+                        label_str = categories[label - 1]['name']
+                        score = scores[index]
+
+                        # Ignore this roi if the score is less than the provided threshold
+                        if score < score_threshold:
+                            continue
+                            
+                        # Translate roi coordinates into combined image coordinates
+                        x0 = x0 * image.width
+                        y0 = y0 * image.height
+                        x1 = x1 * image.width
+                        y1 = y1 * image.height
+                        
+                        if y0 > max_height or y1 > max_height:
+                            continue
+                        elif y0 < min_height or y1 < min_height:
+                            continue
+
+                        # output
+                        roi_file.write("{},{},{},{},{},{},{},{}\n".format(os.path.basename(im_list[i]), x0, y0-translation, x1, y1-translation,
+                                label, label_str, score))
+                translation += img_height
+            
+            # Move finished images to output_path
+            for i in range(len(im_list)):
+                os.rename(im_list[i], os.path.join(output_path, os.path.basename(im_list[i])))
+                
+            end_time = datetime.now()
+            inference_time = end_time - start_time
+            inference_time = int(inference_time.total_seconds() * 1000)
+            time_per_image = int(inference_time / len(im_list))
+            for i in range(len(im_list)):
+                time_file.write("{}&&".format(os.path.basename(im_list[i])))
+            time_file.write(",{},{},{}\n".format(inference_time, len(im_list), time_per_image))
+            total_time += inference_time
+    
+    total_time_file_path = os.path.join(output_path, "total_time.txt")
+    with open(total_time_file_path, "w") as total_time_file:
+        total_time_file.write("Total inference and I/O time: {} ms".format(total_time))
 
 if __name__ == '__main__':
 
@@ -247,42 +254,48 @@ if __name__ == '__main__':
     parser.add_argument('--score', type=float, help='Score threshold to include in csv file', required=False, default=0.0)
     parser.add_argument('--num_classes', type=int, help='Number of classes', required=True, default=2)
     parser.add_argument('--num_imgs', type=int, help='Number of images to combine', required=False, default=1)
+    parser.add_argument('--status', help='file path for predict exit status file', required=False, default=None)
     parser.add_argument('--continuous', action='store_true', help='Whether to continuously load test images and perform prediction', required=False, default=False)
     args = vars(parser.parse_args())
 
-    # Path to frozen detection graph. This is the actual model that is used for the object detection
-    frozen_graph_path = args['graph']
-    detection_graph = load_frozen_graph(frozen_graph_path)
+    try:
+        # Path to frozen detection graph. This is the actual model that is used for the object detection
+        frozen_graph_path = args['graph']
+        detection_graph = load_frozen_graph(frozen_graph_path)
 
-    # List of the strings that is used to add correct label for each box
-    labels_map_path = args['labels']
+        # List of the strings that is used to add correct label for each box
+        labels_map_path = args['labels']
 
-    # The threshold of including the detected roi in the output csv
-    score_threshold = args['score']
+        # The threshold of including the detected roi in the output csv
+        score_threshold = args['score']
 
-    # Getting classes strings from the label map
-    num_classes = args['num_classes']
-    label_map = label_map_util.load_labelmap(labels_map_path)
-    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=num_classes,
-                                                                use_display_name=True)
+        # Getting classes strings from the label map
+        num_classes = args['num_classes']
+        label_map = label_map_util.load_labelmap(labels_map_path)
+        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=num_classes,
+                                                                    use_display_name=True)
 
-    # Loading the path to images to perform prediction on
-    test_images_directory = args['prediction_in']
+        # Loading the path to images to perform prediction on
+        test_images_directory = args['prediction_in']
 
-    # Output directory for the csv files
-    output_path = args['prediction_out']
+        # Output directory for the csv files
+        output_path = args['prediction_out']
+        
+        # Number of images to combine
+        num_imgs = args['num_imgs']
 
-    # File to save prediction time in
-    f = open(output_path + "/prediction_time.txt", "w")
-    
-    # Number of images to combine
-    num_imgs = args['num_imgs']
+        while True:
+            # Performing the prediction and producing the csv files
+            predict_on_images(test_images_directory, detection_graph, output_path, score_threshold, categories, num_imgs)
 
-    while True:
-        # Performing the prediction and producing the csv files
-        predict_on_images(test_images_directory, detection_graph, output_path, score_threshold, categories, f, num_imgs)
-
-        # Exit if not continuous
-        if not args['continuous']:
-            break
-    f.close()
+            # Exit if not continuous
+            if not args['continuous']:
+                break
+        if args['status'] is not None:
+            with open(args['status'], 'w') as f:
+                f.write("Success")
+    except Exception as e:
+        print(e)
+        if args['status'] is not None:
+            with open(args['status'], 'w') as f:
+                f.write(str(e))
