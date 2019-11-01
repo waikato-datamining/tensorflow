4e340f31019a75d3d3e61678d4d9125be3dcbf14
Peter Reutemann <fracpete@gmail.com>
2019-11-01 14:23:44 +1300

    added CHANGES/DESCRIPTION rest files

--- image_classification/setup.py
+++ image_classification/setup.py
@@ -3,11 +3,24 @@
 
 from setuptools import setup, find_namespace_packages
 
+def _read(f) -> bytes:
+    """
+    Reads in the content of the file.
+    :param f: the file to read
+    :type f: str
+    :return: the content
+    :rtype: str
+    """
+    return open(f, 'rb').read()
+
 
 setup(
     name="wai.tfimageclass",
     description="Image classification using tensorflow.",
-    url="https://github.com/waikato-datamining/tensorflow/image_classification",
+    long_description=(
+        _read('DESCRIPTION.rst') + b'\n' +
+        _read('CHANGES.rst')).decode('utf-8'),
+    url="https://github.com/waikato-datamining/tensorflow/tree/master/image_classification",
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
