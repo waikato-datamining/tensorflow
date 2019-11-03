8c35fa63077fa0fa779c17eeb499e9a4e326f97c
hishamjuve <hisham_abdelqader@yahoo.com>
2019-11-04 12:16:30 +1300

    updated Dockerfile to move all shell scripts to /usr/bin after pulling. Fixed all shell scripts.

--- object_detection/2019-08-31/objdet_export.sh
+++ object_detection/2019-08-31/objdet_export.sh
@@ -2,4 +2,3 @@
 
 cd /opt/tensorflow/models/research
 python object_detection/export_inference_graph.py "$@"
-cd /opt/tensorflow/object_detection/2019-08-31
--- object_detection/2019-08-31/objdet_train.sh
+++ object_detection/2019-08-31/objdet_train.sh
@@ -2,4 +2,3 @@
 
 cd /opt/tensorflow/models/research
 python object_detection/model_main.py "$@"
-cd /opt/tensorflow/object_detection/2019-08-31
