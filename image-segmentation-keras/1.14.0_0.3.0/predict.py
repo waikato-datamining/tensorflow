00500a92c72ce78dd75cbafbb2bf9f065267b005
Peter Reutemann <fracpete@gmail.com>
2020-12-04 08:53:40 +1300

    switched to simple-file-poller

--- image-segmentation-keras/1.14.0_0.3.0/predict.py
+++ image-segmentation-keras/1.14.0_0.3.0/predict.py
@@ -14,6 +14,7 @@ from keras_segmentation.data_utils.data_loader import get_image_array
 from keras_segmentation.models.config import IMAGE_ORDERING
 from PIL import Image
 import random
+from sfp import Poller
 
 # colors taken from:
 # conservative 8-color palettes for color blindness
@@ -35,7 +36,7 @@ b = [random.randint(0,255) for _ in range(num_colors)]
 for rgb in zip(r, g, b):
     class_colors.extend(rgb)
 
-SUPPORTED_EXTS = [".jpg", ".jpeg", ".png", ".bmp"]
+SUPPORTED_EXTS = [".jpg", ".jpeg"]
 """ supported file extensions (lower case). """
 
 MAX_INCOMPLETE = 3
@@ -103,7 +104,47 @@ def predict(model, inp, out_fname=None, colors=None, verbose=False):
     return pr, pr_mask
 
 
-def predict_on_images(model, input_dir, output_dir, tmp_dir, delete_input, clash_suffix="-in", colors=None, verbose=False):
+def check_image(fname, poller):
+    """
+    Check method that ensures the image is valid.
+
+    :param fname: the file to check
+    :type fname: str
+    :param poller: the Poller instance that called the method
+    :type poller: Poller
+    :return: True if complete
+    :rtype: bool
+    """
+    result = auto.is_image_complete(fname)
+    poller.debug("Image complete:", fname, "->", result)
+    return result
+
+
+def process_image(fname, output_dir, poller):
+    """
+    Method for processing an image.
+
+    :param fname: the image to process
+    :type fname: str
+    :param output_dir: the directory to write the image to
+    :type output_dir: str
+    :param poller: the Poller instance that called the method
+    :type poller: Poller
+    :return: the list of generated output files
+    :rtype: list
+    """
+    result = []
+    try:
+        out_file = os.path.join(output_dir, os.path.basename(fname))
+        predict(poller.model, fname, out_fname=out_file, colors=poller.colors, verbose=poller.verbose)
+        result.append(out_file)
+    except:
+        poller.log("Failed to process image: %s\n%s" % (fname, traceback.format_exc()))
+    return result
+
+
+def predict_on_images(model, input_dir, output_dir, tmp_dir, delete_input,
+                      colors=None, continuous=False, verbose=False):
     """
     Performs predictions on images found in input_dir and outputs the prediction PNG files in output_dir.
 
@@ -116,17 +157,13 @@ def predict_on_images(model, input_dir, output_dir, tmp_dir, delete_input, clash
     :type tmp_dir: str
     :param delete_input: whether to delete the input images rather than moving them to the output directory
     :type delete_input: bool
-    :param clash_suffix: the suffix to use for clashes, ie when the input is already a PNG image
-    :type clash_suffix: str
     :param colors: the list of colors to use (flat list of r,g,b values, eg "0, 0, 0, 127, 127, 127"), will get padded to 256 triplets
     :type colors: list
+    :param continuous: whether to poll for files continuously
+    :type continuous: bool
     :param verbose: whether to output more logging information
     :type verbose: bool
     """
-
-    # counter for keeping track of images that cannot be processed
-    incomplete_counter = dict()
-    num_imgs = 1
 
     # ensure colors list has correct length for PNG palette
     if colors is not None:
@@ -135,80 +172,19 @@ def predict_on_images(model, input_dir, output_dir, tmp_dir, delete_input, clash
         if len(colors) > 768:
             colors = colors[0:768]
 
-    while True:
-        start_time = datetime.now()
-        im_list = []
-        # Loop to pick up images equal to num_imgs or the remaining images if less
-        for image_path in os.listdir(input_dir):
-            # Load images only
-            ext_lower = os.path.splitext(image_path)[1]
-            if ext_lower in SUPPORTED_EXTS:
-                full_path = os.path.join(input_dir, image_path)
-                if auto.is_image_complete(full_path):
-                    im_list.append(full_path)
-                else:
-                    if not full_path in incomplete_counter:
-                        incomplete_counter[full_path] = 1
-                    else:
-                        incomplete_counter[full_path] = incomplete_counter[full_path] + 1
-
-            # remove images that cannot be processed
-            remove_from_blacklist = []
-            for k in incomplete_counter:
-                if incomplete_counter[k] == MAX_INCOMPLETE:
-                    print("%s - %s" % (str(datetime.now()), os.path.basename(k)))
-                    remove_from_blacklist.append(k)
-                    try:
-                        if delete_input:
-                            print("  flagged as incomplete {} times, deleting\n".format(MAX_INCOMPLETE))
-                            os.remove(k)
-                        else:
-                            print("  flagged as incomplete {} times, skipping\n".format(MAX_INCOMPLETE))
-                            os.rename(k, os.path.join(output_dir, os.path.basename(k)))
-                    except:
-                        print(traceback.format_exc())
-
-            for k in remove_from_blacklist:
-                del incomplete_counter[k]
-
-            if len(im_list) == num_imgs:
-                break
-
-        if len(im_list) == 0:
-            time.sleep(1)
-            break
-        else:
-            print("%s - %s" % (str(datetime.now()), ", ".join(os.path.basename(x) for x in im_list)))
-
-        try:
-            for i in range(len(im_list)):
-                parts = os.path.splitext(os.path.basename(im_list[i]))
-                if tmp_dir is not None:
-                    out_file = os.path.join(tmp_dir, parts[0] + ".png")
-                else:
-                    out_file = os.path.join(output_dir, parts[0] + ".png")
-                #model.predict_segmentation(inp=im_list[i], out_fname=out_file)
-                predict(model, im_list[i], out_fname=out_file, colors=colors, verbose=verbose)
-        except:
-            print("Failed processing images: {}".format(",".join(im_list)))
-            print(traceback.format_exc())
-
-        # Move finished images to output_path or delete it
-        for i in range(len(im_list)):
-            if delete_input:
-                os.remove(im_list[i])
-            else:
-                # PNG input clashes with output, append suffix
-                if im_list[i].lower().endswith(".png"):
-                    parts = os.path.splitext(os.path.basename(im_list[i]))
-                    os.rename(im_list[i], os.path.join(output_dir, parts[0] + clash_suffix + parts[1]))
-                else:
-                    os.rename(im_list[i], os.path.join(output_dir, os.path.basename(im_list[i])))
-
-        end_time = datetime.now()
-        inference_time = end_time - start_time
-        inference_time = int(inference_time.total_seconds() * 1000)
-        print("  Inference + I/O time: {} ms\n".format(inference_time))
+    poller = Poller()
+    poller.model = model
+    poller.colors = colors
+    poller.input_dir = input_dir
+    poller.output_dir = output_dir
+    poller.tmp_dir = tmp_dir
+    poller.delete_input = delete_input
+    poller.verbose = verbose
+    poller.progress = True
+    poller.check_file = check_image
+    poller.process_file = process_image
+    poller.continuous = continuous
+    poller.poll()
 
 
 if __name__ == '__main__':
@@ -221,7 +197,6 @@ if __name__ == '__main__':
     parser.add_argument('--prediction_tmp', help='Path to the temporary csv files folder', required=False, default=None)
     parser.add_argument('--continuous', action='store_true', help='Whether to continuously load test images and perform prediction', required=False, default=False)
     parser.add_argument('--delete_input', action='store_true', help='Whether to delete the input images rather than move them to --prediction_out directory', required=False, default=False)
-    parser.add_argument('--clash_suffix', help='The file name suffix to use in case the input file is already a PNG and moving it to the output directory would overwrite the prediction PNG', required=False, default="-in")
     parser.add_argument('--memory_fraction', type=float, help='Memory fraction to use by tensorflow, i.e., limiting memory usage', required=False, default=0.5)
     parser.add_argument('--colors', help='The list of colors (RGB triplets) to use for the PNG palette, e.g.: 0,0,0,255,0,0,0,0,255 for black,red,blue', required=False, default=None)
     parser.add_argument('--verbose', action='store_true', help='Whether to output more logging info', required=False, default=False)
@@ -246,12 +221,8 @@ if __name__ == '__main__':
             colors = [int(x) for x in colors]
 
         # predict
-        while True:
-            predict_on_images(model, parsed.prediction_in, parsed.prediction_out, parsed.prediction_tmp,
-                              parsed.delete_input, clash_suffix=parsed.clash_suffix, colors=colors,
-                              verbose=parsed.verbose)
-            if not parsed.continuous:
-                break
+        predict_on_images(model, parsed.prediction_in, parsed.prediction_out, parsed.prediction_tmp,
+                          parsed.delete_input, colors=colors, continuous=parsed.continuous, verbose=parsed.verbose)
 
     except Exception as e:
         print(traceback.format_exc())
